a18fbba8ef8177345b62e9d66fc710f9d2db7c3d
Pr3ucando <pr3ucando@gmail.com>
2022-07-24 09:23:47 +0530

    new

---
+++ src/org/test/Animal.java
@@ -0,0 +1,8 @@
+package org.test;
+
+public abstract class Animal {
+	public abstract void dog();
+	public abstract void cat();
+	
+
+}
---
+++ src/org/test/Domesticanimal.java
@@ -0,0 +1,24 @@
+package org.test;
+
+public class Domesticanimal extends Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Domesticanimal d=new Domesticanimal();
+		d.cat();
+		d.dog();
+	}
+
+	@Override
+	public void dog() {
+		// TODO Auto-generated method stub
+	System.out.println("Dog is Barking");	
+	}
+
+	@Override
+	public void cat() {
+		// TODO Auto-generated method stub
+	System.out.println("Cat is meow");	
+	}
+
+}
